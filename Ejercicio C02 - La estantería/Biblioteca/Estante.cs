9b85884082565228b07a014cfe93167ac06eedea
Kuro Okami <patrygaly01@gmail.com>
2022-09-05 19:41:31 -0300

    Estanteria

---
+++ Ejercicio C02 - La estantería/Biblioteca/Estante.cs
@@ -0,0 +1,107 @@
+﻿/*
+
+Generar la clase Estante.
+
+https://codeutnfra.github.io/programacion_2_laboratorio_2_apuntes/assets/images/estante-diagram-92964e4b26abc3145cc9d7366cad5bab.png
+
+1. Posee dos atributos privados. Uno será un entero que indicará la ubicación del estante y el otro es un array de tipo "Producto".
+2. El constructor de instancia privado será el único que incializará el array. La sobrecarga pública del constructor inicializará la ubicación del estante, recibiendo como parámetros la capacidad y la ubicación. Reutilizar código.
+3. El método público "GetProductos", retornará el array de productos.
+4. El método público de clase (estático) "MostrarEstante", retornará una cadena con toda la información del estante incluyendo el detalle de cada uno de sus productos. Reutilizar código.
+5. Posee las siguientes sobrecargas de operadores:
+    i. "==": Retornará "true" si es que el producto ya se encuentra en el estante, "false" caso contrario.
+    ii. "+": Retornará "true" y agregará el producto si el estante posee capacidad de almacenar al menos un producto más y dicho producto no se encuentra en el estante, "false" caso contrario. Reutilizar código.
+    iii. "-": Retornará un estante sin el producto, siempre y cuando el producto se encuentre en el listado. Reutilizar código.
+
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion) : this (capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Ubicacion del estante: {e.ubicacionEstante}");
+
+            foreach (Producto producto in e.productos)
+            {
+                sb.AppendLine(Producto.MostrarProducto(producto));
+            }
+
+            return sb.ToString();
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            if (e != p)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos[i] is null)
+                    {
+                        e.productos[i] = p;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            foreach (Producto producto in e.productos)
+            {
+                if (producto == p)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        public static Estante operator -(Estante e, Producto p)
+        {
+            if (e == p)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos[i] == p)
+                    {
+                        e.productos[i] = null;
+                    }
+                }
+            }
+            return e;
+        }
+    }
+}
---
+++ Ejercicio C02 - La estantería/Biblioteca/Producto.cs
@@ -0,0 +1,79 @@
+﻿/*
+Crear un proyecto de tipo biblioteca de clases que contenga la clase Producto.
+
+https://codeutnfra.github.io/programacion_2_laboratorio_2_apuntes/assets/images/producto-diagram-765be2d7770aabd572e603a495bef7a2.png
+
+1. Todos sus atributos son privados.
+2. Posee sólo un constructor de instancia.
+3. El método "GetMarca", retornará el valor correspondiente al atributo "marca".
+4. También poseerá el atributo "codigoDeBarras", el cual se publicará sólo a través de la conversión explícita nombrada más adelante.
+5. El método de clase (estático) "MostrarProducto" es público y retornará una cadena detallando los atributos de la clase.
+6. Posee las siguientes sobrecargas de operadores:
+    i. "explicit": Realizará la conversión de un objeto "Producto" a "string". Sólo retornará el atributo "codigoDeBarras" del producto.
+    ii. "== (Producto, Producto)": Retornará "true" si las marcas y códigos de barra son iguales, "false" caso contrario.
+    iii. "== (Producto, string)": Retornará "true" si la marca del producto coincide con la cadena pasada como argumento, "false" caso contrario.
+
+ */
+
+using System;
+
+namespace Biblioteca
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca, string codigoDeBarra, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigoDeBarra;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static string MostrarProducto (Producto p)
+        {
+            return $"Marca {p.marca} - Codigo {p.codigoDeBarra} - Precio {p.precio}";
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static bool operator == (Producto p1, Producto p2)
+        {
+            if (p1 is not null && p2 is not null)
+            {
+                return p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+        public static bool operator ==(Producto p1, string marca)
+        {
+            return p1.marca == marca;
+        }
+
+        public static bool operator !=(Producto p1, string marca)
+        {
+            return !(p1.marca == marca);
+        }
+    }
+}
